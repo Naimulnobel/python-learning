f69377321557f221f8e26ad0a0aababce88090d9
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-16 12:18:11 +0600

    Add files via upload

---
+++ forloops.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 11:59:36 2019
+
+@author: Student mct
+"""
+print('my name is')
+for i in range(5):
+    print('jimy five time('+str(i)+')')
---
+++ forloops2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 12:07:20 2019
+
+@author: Student mct
+"""
+
+total=0
+for num in range(101):
+    total=total+num
+print(total)
+
+ for i in range(12,16):
+     print(i)
+
+for n in range(5,-1,-1):
+    print(n)
+    
