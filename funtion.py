2233954c534e9679686b058c429f7b92c220df09
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-16 12:50:13 +0600

    Add files via upload

---
+++ funtion.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 12:41:15 2019
+
+@author: Student mct
+"""
+def hello():
+    print('hi')
+    print('i am fine')
+    print('thank you')
+hello()
+
