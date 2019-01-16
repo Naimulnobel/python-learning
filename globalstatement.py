9cb564c6f45cedc650e5b8d35eb1b36110492e24
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-16 14:52:59 +0600

    Add files via upload

---
+++ globalstatement.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 14:43:31 2019
+
+@author: Student mct
+"""
+
+def spam():
+     global eggs 
+     eggs = 'spam'
+     eggs = 'global' 
+     print(eggs)
+spam() 
+   
