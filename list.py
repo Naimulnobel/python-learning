daa32dde43b6b1e99e5f43f05323af815e98ee34
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-16 16:05:02 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 15:47:29 2019
+
+@author: Student mct
+"""
+
+animal=['dog','cat','ant','human']
+print(animal[::-1])
+animal1=['dog','cat','ant','human']
+print(animal1[1:3])
