05363e2c46c10e0a23b62975d2076b728242f3ef
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-16 12:34:52 +0600

    Add files via upload

---
+++ importfuntions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 12:17:01 2019
+
+@author: Student mct
+"""
+import random
+for i in range(5):
+    print(random.randint(1,10))
+    
---
+++ importfuntions2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 12:26:18 2019
+
+@author: Student mct
+"""
+import sys
+while True:
+    print('Type exit to exit')
+    response=input()
+    if response== 'exit':
+        sys.exit()
+print('you typed'+response+'.')  
