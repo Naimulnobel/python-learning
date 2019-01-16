60940a0bedb60f0951a51224434fdde56c394672
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-16 15:26:07 +0600

    Add files via upload

---
+++ game.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 15:15:06 2019
+
+@author: Student mct
+"""
+
+import random 
+secretNumber = random.randint(1, 20)
+print('I am thinking of a number between 1 and 20.')
+for guessesTaken in range(1, 7):
+    print('Take a guess.')   
+    guess = int(input())
+    if guess < secretNumber:       
+        print('Your guess is too low.')   
+    elif guess > secretNumber:      
+        print('Your guess is too high.')    
+    else:        
+       break    
+if guess == secretNumber:    
+    print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!') 
+else:    
+    print('Nope. The number I was thinking of was ' + str(secretNumber))
