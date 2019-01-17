b2521ea9e1dde252232ae26544576429b5e1abb4
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-17 16:32:22 +0600

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 17 15:34:50 2019
+
+@author: Student mct
+"""
+
+spam=('hello',1,2)
+print(spam[1:3])
+print(len(spam))
+print(type(spam))
+list('HELLO')
+
+
