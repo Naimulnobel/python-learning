ae0cfbef677de810322823aa19e48b84dedd350a
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-18 17:41:59 +0600

    Add files via upload

---
+++ class.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 18 16:42:34 2019
+
+@author: NOBEL
+"""
+
+class animal:
+    def cow():
+        print('domestic animal')
+    def dog():
+        print('loyal pet')
+ 
+animal.dog()       
