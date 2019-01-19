4b3fd735a49384d04ee2ea2e10b88d1838eb7b18
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-19 13:12:53 +0600

    Add files via upload

---
+++ stringmanupulation.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 19 12:51:27 2019
+
+@author: Student mct
+"""
+
+print(r'that\'s why i am telling this')
+print('''dear mishad
+      Don't steal anything from any where 
+      your fiend
+      nobel
+      ''')
+text='python'
+print(text[0:2])
+print(text[2:6])
+text1=text.upper()
+print(text1)
+text2=text.lower()
+print(text2)
